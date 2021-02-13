ac70c24e048e0d6c19c01a53b03c21ef3a49d439
helloworld-html <helloworld.pdf@gmail.com>
2021-02-13 16:19:35 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 'use strict'
 
+let table = document.querySelector('table')
+
 document.querySelector('header>button').addEventListener('click', function() {
-  document.querySelector('header').style.animation = 'slide 1s';
-  document.querySelector('header').style.animationIterationCount = '1'
+  document.querySelector('header').style.animation = 'slide 1s forwards';
+  this.style.display = "none";
+  document.querySelector('header>div').style.backgroundColor = 'rgba(255, 255, 255, 0)';
 })
+
+function addCell() {
+  let row = table.insertRow();
+  let cell1 = row.insertCell();
+  cell1.innerHTML = "A";
+}
