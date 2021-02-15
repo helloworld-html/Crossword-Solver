3e6d2a9b0319214dc521b4b77e838db8d105db74
helloworld-html <helloworld.pdf@gmail.com>
2021-02-15 20:15:13 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict'
 
 let table = document.querySelector('table');
+let cells = document.querySelectorAll('td > input');
 
 document.querySelector('header > button').addEventListener('click', function() {
   document.querySelector('header').style.animation = 'slide 1s forwards';
@@ -8,3 +9,7 @@ document.querySelector('header > button').addEventListener('click', function() {
   document.querySelector('header > div').style.backgroundColor = 'rgba(255, 255, 255, 0)';
   document.querySelector('main').style.display = 'block'
 })
+
+for (let x of cells) {
+  x.maxLength = 1;
+}
