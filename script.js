c62e2859f0f24ffc93f7db9f1e0474088b9987ba
helloworld-html <helloworld.pdf@gmail.com>
2021-02-17 16:05:02 +0100

    update

--- script.js
+++ script.js
@@ -5,18 +5,31 @@ let cells = document.querySelectorAll('td > input');
 
 document.querySelector('header > button').addEventListener('click', function() {
   document.querySelector('header').style.animation = 'slide 1s forwards';
-  this.style.display = "none";
+  this.style.display = 'none';
   document.querySelector('header > div').style.backgroundColor = 'rgba(255, 255, 255, 0)';
   document.querySelector('main').style.display = 'block'
 })
 
-for (let x of cells) {
-  const index = [...cells].indexOf(x);
-  x.maxLength = 1;
-  x.type = 'text';
-  x.onkeyup = function() {
-    if (this.value != '' && cells[index + 1]) {
-      cells[index + 1].focus();
+function AccessTableInput() {
+  for (let x of cells) {
+    const index = [...cells].indexOf(x);
+    x.maxLength = 1;
+    x.type = 'text';
+    x.onkeyup = function() {
+      if (this.value != '' && cells[index + 1]) {
+        cells[index + 1].focus();
+      }
     }
   }
+}
+
+AccessTableInput()
+
+function ApplyTableChanges() {
+  let checkbox = document.getElementById('reset');
+  if (document.getElementById('reset').checked) {
+    for (let x of cells) { x.value = '' }
+  }
+  checkbox.checked = false;
+  document.querySelector('section').style.display = 'none';
 }
