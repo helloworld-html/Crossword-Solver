1324fd0b75d103f179819c867c443152736a828d
helloworld-html <helloworld.pdf@gmail.com>
2021-03-09 19:15:36 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
 
-let table = document.querySelector('table');
-let inputSearch = document.getElementById('inputSearch');
+const table = document.querySelector('table');
+const inputSearch = document.getElementById('inputSearch');
 
 document.querySelector('header > button').addEventListener('click', function() {
   document.querySelector('header').style.animation = 'slide 1s forwards';
@@ -13,14 +13,13 @@ document.querySelector('header > button').addEventListener('click', function() {
 
 function GenerateTable(x, y) {
   for (let column = 0; column < x; column++) {
-    let newRow = table.insertRow();
+    const newRow = table.insertRow();
     for (let row = 0; row < y; row++) {
-      let input = document.createElement('INPUT');
-      newRow.insertCell().appendChild(input);
+      newRow.insertCell().appendChild(document.createElement('INPUT'));
     }
   }
-  let cells = document.querySelectorAll('td > input');
-  for (let x of cells) {
+  const cells = document.querySelectorAll('td > input');
+  for (const x of cells) {
     x.maxLength = 1;
     x.type = 'text';
     const index = [...cells].indexOf(x);
@@ -35,13 +34,13 @@ function GenerateTable(x, y) {
 GenerateTable(5, 5)
 
 function ApplyTableChanges() {
-  let valueColumns = document.getElementById('columns').value;
-  let valueRows = document.getElementById('rows').value;
+  const valueColumns = document.getElementById('columns').value;
+  const valueRows = document.getElementById('rows').value;
   if (valueColumns < 5 || valueRows < 5 || valueRows > 25) {
     alert("Please choose a value between 5 and 20");
     return false
   }
-  let confirmChange = confirm("This will automatically delete all table values");
+  const confirmChange = confirm("This will automatically delete all table values");
   if (confirmChange == false) {
     document.querySelector('section').style.display = 'none';
     return false
@@ -60,10 +59,21 @@ inputSearch.addEventListener('keypress', function(event) {
 })
 
 function SubmitSearch() {
-  for (const row of Object.values(table.rows)) {
-    for (const cell in Object.values(row.cells)) {
-      const y = cell.firstChild.value;
-      console.log(y)
+  const array = [];
+  for (const row of table.rows) {
+    for (const cell of row.cells) {
+      const valueCell = cell.firstChild.value;
+      array.push(valueCell)
     }
   }
-} //YOUR CODE MATTHAIS
+  let values = chunkArray(array, table.rows[0].cells.length);
+  console.log(values);
+}
+
+function chunkArray(x, chunk) {
+  const array = [];
+  while (x.length) {
+    array.push(x.splice(0, chunk));
+  }
+  return array;
+}
